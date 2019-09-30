542a5a9fa6a9757f8aff373bca5caf46eb473c26
meilcli <ics.sora.meil@gmail.com>
2019-09-30 13:08:05 +0900

    add

---
+++ cocoapods-update-check-action-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cocoapods-update-check-action-example
+//
+//  Created by k.mizutani on 2019/09/30.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
